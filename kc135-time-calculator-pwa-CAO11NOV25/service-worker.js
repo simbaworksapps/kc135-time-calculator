77d581c36d6b8e084a597f5a5e6e25a54f580ba5
michaelrumpsa <michaelrumpsa@gmail.com>
2025-11-01 19:20:58 -0400

    Delete kc135-time-calculator-pwa-CAO11NOV25 directory

--- kc135-time-calculator-pwa-CAO11NOV25/service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='kc135-pwa-v5_9_5';
-const ASSETS=['./','./index.html','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./assets/simba.jpg'];
-self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); });
-self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))); });
-self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
