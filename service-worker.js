4dcc3d4dc176c538ea26ea82e21b44d6011854d8
michaelrumpsa <michaelrumpsa@gmail.com>
2025-11-01 22:02:40 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kc135-pwa-v5_9_5';
+const CACHE_NAME = "kc135-pwa-v5_9_6";
 const ASSETS=['./','./index.html','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./assets/simba.jpg'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))); });
